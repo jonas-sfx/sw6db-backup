5b027a6266ff35e48c1d49a14553531c4c46ca1d
Jonas Hess <jonas@sfxonline.de>
2022-11-02 22:08:37 +0100

    add dump- and restore-scripts

---
+++ dump.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+## filename     dump.sh
+## description: create dumps with week/weekday as filename
+## author:      jonas@sfxonline.de
+## =======================================================================
+
+cd "$(dirname "$0")"
+
+# CREDENTIALS
+MYSQL=$(grep -Po 'DATABASE_URL=\K.*' ../project-root/.env)
+USER="$(echo $MYSQL | grep -Po 'mysql://\K[^:]+')"
+pwdgrep="mysql://$USER:\K[^@]+"
+PASSWORD="$(echo $MYSQL | grep -Po $pwdgrep)"
+HOST="$(echo $MYSQL | grep -oP '@[^:\/]+' | cut -d@ -f2)"
+PORT="$(echo $MYSQL | grep -oP '@.*:[0-9]+' | cut -d: -f2)"
+DB="$(echo $MYSQL | grep -oP '/[^\/]+$' | cut -d/ -f2)"
+
+STR_WEEKDAY=$(date +%a)".tar.gz"
+STR_WEEK=$(date +%U)".tar.gz"
+MYSQLDUMP='/usr/local/mysql/bin/mysqldump'
+
+# CHECK FOR EXISTING FILES
+MAKEW=0
+MAKED=0
+
+if test -f "archive/$STR_WEEKDAY"; then
+    echo "$STR_WEEKDAY exists."
+    if [ `stat --format=%Y archive/$STR_WEEKDAY` -le $(( `date +%s` - 3600*24*6 )) ]; then 
+        echo "$STR_WEEKDAY to old."
+        MAKED=1
+    fi
+else
+    echo "$STR_WEEKDAY does not exist."
+    MAKED=1
+fi
+
+if test -f "archive/$STR_WEEK"; then
+    echo "$STR_WEEK exists."
+    if [ `stat --format=%Y archive/$STR_WEEK` -le $(( `date +%s` - 3600*24*350 )) ]; then 
+        echo "$STR_WEEK to old."
+        MAKEW=1
+    fi
+else
+    echo "$STR_WEEK does not exist."
+    MAKEW=1
+fi
+
+# MAKE THE DUMPs you need.
+if [[ $(($MAKEW  + $MAKED)) -gt 0 ]]; then
+  $MYSQLDUMP -u"$USER" -p"$PASSWORD" -h"$HOST" -P $PORT --no-tablespaces --hex-blob $DB > last-dump.sql
+
+  if [ $MAKEW -gt 0 ]; then
+    tar -czf "archive/$STR_WEEK" last-dump.sql
+  fi
+
+  if [ $MAKED -gt 0 ]; then
+    tar -czf "archive/$STR_WEEKDAY" last-dump.sql
+  fi
+
+  rm last-dump.sql
+fi
+
---
+++ restore.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+## filename     restore.sh
+## description: restore dumps with week/weekday as filename
+## author:      jonas@sfxonline.de
+## =======================================================================
+
+cd "$(dirname "$0")"
+
+# CREDENTIALS
+MYSQL=$(grep -Po 'DATABASE_URL=\K.*' ../project-root/.env)
+USER="$(echo $MYSQL | grep -Po 'mysql://\K[^:]+')"
+pwdgrep="mysql://$USER:\K[^@]+"
+PASSWORD="$(echo $MYSQL | grep -oP $pwdgrep)"
+HOST="$(echo $MYSQL | grep -oP '@[^:\/]+' | cut -d@ -f2)"
+PORT="$(echo $MYSQL | grep -oP '@.*:[0-9]+' | cut -d: -f2)"
+DB="$(echo $MYSQL | grep -oP '/[^\/]+$' | cut -d/ -f2)"
+
+if test -f "archive/$1.tar.gz"; then
+    echo "Backup $1.tar.gz exists."
+    cd archive/
+    tar -xzf $1.tar.gz
+    sed 's/\sDEFINER=`[^`]*`@`[^`]*`//g' -i last-dump.sql
+    mysql  -u"$USER" -p"$PASSWORD" -h"$HOST" -P $PORT $DB < last-dump.sql
+    # echo "mysql  -u\"$USER\" -p\"$PASSWORD\" -h\"$HOST\" -P $PORT $DB < last-dump.sql"
+    rm last-dump.sql
+    exit 0
+else
+  echo "Backup $1.tar.gz does not exist."
+fi
